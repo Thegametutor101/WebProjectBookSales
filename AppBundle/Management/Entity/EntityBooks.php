7be717ad3e0dd898ef80ff2e3c96f6c14f9289cc
Daniel <theendercraftgaming@gmail.com>
2020-11-02 12:36:21 -0500

    Optimized printing

--- AppBundle/Management/Entity/EntityBooks.php
+++ AppBundle/Management/Entity/EntityBooks.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once('ProjectConstants.php');
 
 class EntityBooks
 {
--- AppBundle/Management/loadBooks.php
+++ AppBundle/Management/loadBooks.php
@@ -1,18 +1,6 @@
 <?php
-require_once ("ProjectConstants.php");
+require_once('Entity/EntityBooks.php');
 $lines = array();
-try
-{
-//    new PDO("mysql:host=206.167.140.56;dbname=420505ri_gr06;port=3306,charset=utf8","1763237","1763237");
-    $constants = new ProjectConstants();
-    $connexion = $constants->getConnexion();
-
-    $request = "SELECT * FROM book";
-    $result = $connexion->query($request);
-    $lines = $result->fetchAll();
-
-    echo json_encode(array( "OK", "lines" => $lines));
-}
-catch(PDOException $e) {
-    echo json_encode(array("ERROR", "message" => "Échec de connexion à la base de données: " . $e->getMessage()));
-}
+$entityBooks = new EntityBooks();
+$lines = $entityBooks->getBooks();
+echo json_encode(array("lines" => $lines));
